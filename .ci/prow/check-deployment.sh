73b1f8a9de29d8f04024537c53d59dddaf8df4ce
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2022-07-27 10:52:46 -0300

    [WIP] Route check

--- .ci/prow/check-deployment.sh
+++ .ci/prow/check-deployment.sh
@@ -4,6 +4,10 @@
 # 1..5 - a wrong configuration on root route
 # 6..10 - a wrong configuration on /api/v3/ route
 # 11..15 - a wrong configuration on /auth/login/ route
+# 16..20 - a wrong configuration on /pulp/content/ route
+# 21 - a wrong certificate configured on route
+# 22 - a wrong key cert configured on route
+# 23 - the hostname does not match certificate subject SAN or common name
 
 set -e #fail in case of non zero return
 
@@ -59,6 +63,28 @@ if [ ${core_content[3]} != "edge" ] ; then exit 19 ; fi
 if [ ${core_content[4]} != "${PULP_INSTANCE}-content-svc" ] ; then exit 20 ; fi
 echo "/pulp/content/ path OK..."
 
+# should also tests things like:
+# - if deployment type=galaxy and /pulp_cookbook/content/ route is present ERROR
+# - if deployment type=galaxy and /pypi/ route is present ERROR
+# ...
 
+# check route certificates
+route_secret=$(oc -n $OPERATOR_NAMESPACE get pulp $PULP_INSTANCE -ojsonpath='{.spec.route_tls_secret}')
+if [[ $route_secret != "" ]] ; then
+  routes=$(oc -n $OPERATOR_NAMESPACE get routes -oname)
+  for route in $routes ; do
+    route_certificate=$(oc -n $OPERATOR_NAMESPACE get route $route -ogo-template='{{.spec.tls.certificate}}{{"\n"}}' | md5sum)
+    secret_certificate=$(oc -n $OPERATOR_NAMESPACE extract "secret/$route_secret" --keys=tls.crt  --to=- | md5sum)
+    if [[ $secret_certificate != $route_certificate ]] ; then exit 21 ; fi
+
+    route_cert_key=$(oc -n $OPERATOR_NAMESPACE get route $route -ogo-template='{{.spec.tls.key}}{{"\n"}}' | md5sum)
+    secret_cert_key=$(oc -n $OPERATOR_NAMESPACE extract "secret/$route_secret" --keys=tls.key  --to=- | md5sum)
+    if [[ $route_cert_key != $secret_cert_key ]] ; then exit 22 ; fi
+  done
+fi
+
+# validate route hostname and certificate
+check_host_cert=$(echo | openssl s_client -verify_hostname  "${PULP_INSTANCE}.${INGRESS_DEFAULT_DOMAIN}"  -connect "${PULP_INSTANCE}.${INGRESS_DEFAULT_DOMAIN}":443 2>/dev/null | awk -F': ' '/Verification/ {print $2}')
+if [[ "$check_host_cert" != "OK" ]] ; then exit 23 ; fi
 
 echo "All paths OK!"
